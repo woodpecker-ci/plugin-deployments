aec7077029abccb98e78a8fc7da798e7131e5364
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-02-16 12:37:06 +0100

    fix deployment url and name

--- plugin.go
+++ plugin.go
@@ -57,7 +57,7 @@ func (p *Plugin) execute(ctx context.Context) error {
 
 	switch action {
 	case "create":
-		err := _forge.CreateDeployment(ctx, p.Metadata.Repository, deploymentURL, deploymentName, &p.Metadata.Curr)
+		err := _forge.CreateDeployment(ctx, p.Metadata.Repository, deploymentName, deploymentURL, &p.Metadata.Curr)
 		if err != nil {
 			return err
 		}
